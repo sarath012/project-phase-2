9d2f7654ae35ef8cc00f41faba139e2996f6a9fb
sarathtechmojo <sarath.surendran@techmojo.in>
2022-07-27 12:58:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Components/Login/Login";
+import Products from "./Components/Products/Products";
+import { appContext } from "./Context/AppContext";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [sortArray, setSortArray] = useState("");
+  const [titleFilter,setTitleFilter] = useState("");
+  const [filter, setFilter] = useState({
+    brand: [],
+    category: [],
+    stock: true
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <appContext.Provider
+        value={{
+          isLoggedIn,
+          setIsLoggedIn,
+          selectedProduct,
+          setSelectedProduct,
+          sortArray,
+          setSortArray,
+          titleFilter,
+          setTitleFilter,
+          filter, 
+          setFilter
+        }}
+      >
+        {isLoggedIn ? <Products /> : <Login />}
+      </appContext.Provider>
     </div>
   );
 }
